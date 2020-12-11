e3fd8a6e638f11060ded61e3e98ac3441ce186c3
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-11 19:03:51 +0900

    Learn Storybook For Building Design System

---
+++ storybook-tdd-tutorial/src/stories/2-App.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { withKnobs, text } from "@storybook/addon-knobs";
+import App from "../App";
+export default {
+  title: "App",
+  component: App,
+  decorators: [withKnobs],
+};
+export const content = () => {
+  const content = text("content", "Hello storybook!");
+  return <App content={content} />;
+};
