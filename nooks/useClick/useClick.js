5cecad22bdac93f917e4587dd9253c1d066e2335
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-01-19 23:32:44 +0900

    Add files via upload

---
+++ nooks/useClick/useClick.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useRef } from "react";
+
+//useRef가 있고 element를 똑같이 return해줘서 title과 상호작용 가능
+export const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    //dependency가 없을 때 useEffect가 mount 또는 update 되면 아래를 호출
+    //(없다면 componentDidMount때만 호출)
+    //reference 안에 element.current가 있는지 확인
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    //componentWillUnmount 되면 알아서 아래를 리턴함
+    //알아서 EventListner 정리하기
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  //dependency가 있다면 update될 때마다 EventListener가 생길 것임
+  //no Dependencies라서 Update된 이후는 생각할 필요 없음
+  return element;
+};
