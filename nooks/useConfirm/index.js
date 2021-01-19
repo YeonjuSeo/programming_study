8330675598a99a365a64db8d878c79809c9c6d73
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-01-19 23:33:21 +0900

    Create index.js

---
+++ nooks/useConfirm/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useRef } from "react";
+
+//사용자가 뭔가를 하기 전에 정말로 그것을 하기 원하는지 확인하는 Hooks
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onCancel !== "function") {
+    //onCancel은 꼭 있어야하는 것은 아님
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
+    }
+  };
+  return confirmAction;
+};
+
+export default function App() {
+  const deleteWorld = () => console.log("Deleting the world...");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+}
