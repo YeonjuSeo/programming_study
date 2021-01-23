e60c15fc4de03fb9ec1fe7072c8dbd52a6f16394
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-01-24 02:18:15 +0900

    Create index.js

---
+++ nooks/useFullscreen/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useRef } from "react";
+import useFullscreen from "./useFullscreen.js";
+
+export default function App() {
+  const onFullS = (isFull) => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <div ref={element}>
+        <img src="https://cdn.pixabay.com/photo/2020/12/03/16/58/sunset-5801050__340.jpg" />
+        <button onClick={exitFull}>Exit Fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make Fullscreen</button>
+    </div>
+  );
+}
