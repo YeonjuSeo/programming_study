c8c8f06a03ccfece360cda5c71da14ef332579ce
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 00:37:36 +0900

    Update index.js

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -6,14 +6,30 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
+  //action에 reducer가 들어간다
   switch (action.type) {
     case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() };
       // 여기서 state를 절대 mutation은 하지 말아야한다. ex.state.push(text);
       // mutate하는 대신 항상 새로운 state를 리턴해야한다.
-      return [...state, { text: action.text, id: Date.now() }];
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return [];
+      //splice()는 배열을 변형하므로(배열 내 삭제) 새로운 배열을 반환하는 filter()를 사용
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -23,12 +39,39 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  // store를 변경할 수 있는 유일한 방법: action을 보내기
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = ""; //clean the whole list
+  // create new list
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+    li.appendChild(btn);
+  });
+};
+
+store.subscribe(paintToDos); //toDO의 변화에 맞게 list를 repainting
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  // store를 변경할 수 있는 유일한 방법: action을 보내기
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
