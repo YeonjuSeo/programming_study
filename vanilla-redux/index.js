cb4e56759946f46bf1cdd1ac91656b4716233299
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 00:05:22 +0900

    Update index.js

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,38 +1,34 @@
 import { createStore } from "redux"; //redux는 data 관리를 도와주는 역할
-//store는 data, 즉 state를 넣는 곳
-//state는 application에서 바뀌는 data를 말한다.
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-number.innerText = 0;
-
-//data를 modify할 함수 reducer인 countModifier
-//data를 modify 할 수 있는 유일한 함수!!
-const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      // 여기서 state를 절대 mutation은 하지 말아야한다. ex.state.push(text);
+      // mutate하는 대신 항상 새로운 state를 리턴해야한다.
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
   }
 };
 
-const countStore = createStore(countModifier); //data를 저장하는 곳
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+store.subscribe(() => console.log(store.getState()));
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  // store를 변경할 수 있는 유일한 방법: action을 보내기
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-//subscribe는 store 안에 있는 변화들을 알 수 있게 해줌
-countStore.subscribe(onChange);
-
-const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
-};
-
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
