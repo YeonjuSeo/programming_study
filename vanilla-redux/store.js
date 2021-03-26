9450eb61e42d0beb4b847855e5c462d802c5a5e7
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 23:41:35 +0900

    Applying createRecucer and configureStore of React-redux

--- vanilla-redux/store.js
+++ vanilla-redux/store.js
@@ -1,25 +1,22 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      //   console.log(action); //이걸 보면 action.payload가 이전의 text에 해당하는 내용이 있다는 것을 알 수 있음
-      //toolkit을 사용하면 action에 보내고 싶어하는 정보가 무엇이든지 payload와 함께 보내진다.
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      console.log(action);
-      // createAction으로 만들어진 action으로부터 뭔가를 받으면 그건 action.payload로 가므로 action.payload 사용
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// 이전의 reducer는 state를 mutate하지 못하고 매번 새로운 state를 반환해야했지만
+//createReducer를 사용하면 state를 mutate할 수 있다.
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // state를 mutate하는 경우 return 하지 말아야한다.(push는 아무것도 리턴하지 X)
+    // mutate하지만 return하는데 return은 언제나 새로운 state로 해야한다.(filter는 return값이 존재)
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 //subscribe ; 변화가 생길 때마다 Application을 rerender하고싶다!
 
