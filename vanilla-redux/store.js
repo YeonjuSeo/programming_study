fc58f3993057eb8cb986437c9574e71da1dadebc
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 22:51:08 +0900

    After Vanilla-Redux #3.4

---
+++ vanilla-redux/store.js
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id: parseInt(id),
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+//subscribe ; 변화가 생길 때마다 Application을 rerender하고싶다!
+
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
+
+export default store;
