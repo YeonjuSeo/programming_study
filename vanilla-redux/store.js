664b5819a6509731f9d8f279fc7bf62c3ac396b2
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 23:52:04 +0900

    #3.4 Applying createSlice of redux toolkit

--- vanilla-redux/store.js
+++ vanilla-redux/store.js
@@ -1,28 +1,22 @@
-import { createStore } from "redux";
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-// 이전의 reducer는 state를 mutate하지 못하고 매번 새로운 state를 반환해야했지만
-//createReducer를 사용하면 state를 mutate할 수 있다.
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    // state를 mutate하는 경우 return 하지 말아야한다.(push는 아무것도 리턴하지 X)
-    // mutate하지만 return하는데 return은 언제나 새로운 state로 해야한다.(filter는 return값이 존재)
-    state.push({ text: action.payload, id: Date.now() });
+// createSlice는 reducer와 action 둘 다를 제공한다!
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
+// createSlice는 알아서 reducer를 제공
+const store = configureStore({ reducer: toDos.reducer });
 
-//subscribe ; 변화가 생길 때마다 Application을 rerender하고싶다!
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+// toDos의 action에서 add와 remove를 가져다 쓰기
+export const { add, remove } = toDos.actions;
 
 export default store;
