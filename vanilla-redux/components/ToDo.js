bd9aa5747b1c059fc04475e2eabc2c82bc59952c
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 22:49:04 +0900

    After Vanilla-Redux #3.4

---
+++ vanilla-redux/components/ToDo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
+
+function ToDo({ text, onBtnClick, id }) {
+  return (
+    <li>
+      <Link to={`./${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
+    </li>
+  );
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  //ownProps 객체에는 현재 text와 id가 들어있다.
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
