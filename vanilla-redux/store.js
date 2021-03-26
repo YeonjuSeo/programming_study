761f47c22662f2f350758dd7577430113d725801
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-03-26 23:12:53 +0900

    Applying CreateAction of Redux Toolkit

--- vanilla-redux/store.js
+++ vanilla-redux/store.js
@@ -1,28 +1,19 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      //   console.log(action); //이걸 보면 action.payload가 이전의 text에 해당하는 내용이 있다는 것을 알 수 있음
+      //toolkit을 사용하면 action에 보내고 싶어하는 정보가 무엇이든지 payload와 함께 보내진다.
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      console.log(action);
+      // createAction으로 만들어진 action으로부터 뭔가를 받으면 그건 action.payload로 가므로 action.payload 사용
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
