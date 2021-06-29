9a9e103762ee76ef19d4b7e3016408f28a5a34f7
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-06-30 02:16:15 +0900

    Create index.js

---
+++ MovieQL/index.js
@@ -0,0 +1,7 @@
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+});
+server.start(() => console.log("GraphQL Server Running"));
